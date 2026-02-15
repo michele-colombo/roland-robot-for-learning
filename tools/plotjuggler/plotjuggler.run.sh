6459b86ed272ae808ac5c3bab8ba5aade88098d6
michele-colombo <miky.colombo97@gmail.com>
2026-02-15 16:47:32 +0100

    move plotjuggler script and add config

--- tools/plotjuggler/plotjuggler.run.sh
+++ tools/plotjuggler/plotjuggler.run.sh
@@ -1,8 +1,11 @@
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
 xhost +
 docker run -it --rm --privileged \
     --network=host --ipc=host --pid=host \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "${SCRIPT_DIR}":/plotjuggler \
+    --name pj \
 ros-pj:jazzy \
     bash -c \
         "source /opt/ros/jazzy/setup.bash && \

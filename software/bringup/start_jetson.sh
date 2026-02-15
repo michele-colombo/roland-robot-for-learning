b27e7a4a039a823153dd4bdaab3ee2fbabb303f6
michele-colombo <miky.colombo97@gmail.com>
2026-02-15 18:46:55 +0100

    fix bringup after changing device name

--- software/bringup/start_jetson.sh
+++ software/bringup/start_jetson.sh
@@ -2,8 +2,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
 $REPO_ROOT/tools/micro-ros/docker.run.sh -d ./run_agent.sh \
-    serial --dev /dev/ttyUSB0
-
-$REPO_ROOT/tools/micro-ros/docker.run.sh -d ./run_agent.sh \
-    serial --dev /dev/ttyUSB1
-
+    serial --dev /dev/esp32-base

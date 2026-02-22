101b38c0ff1247647627f809f9a724393cd8a45e
michele-colombo <miky.colombo97@gmail.com>
2026-02-22 15:20:31 +0100

    move bringup folder up

--- bringup/start_jetson.sh
+++ bringup/start_jetson.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-REPO_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
+REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 $REPO_ROOT/tools/micro-ros/docker.run.sh -d ./run_agent.sh \
     serial --dev /dev/esp32-base

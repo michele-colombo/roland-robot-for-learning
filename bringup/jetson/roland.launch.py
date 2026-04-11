5cca8540a8eaf2a5ee963086e52eeb61aef97724
michele-colombo <miky.colombo97@gmail.com>
2026-04-11 16:24:41 +0200

    fix: isolate IncludeLaunchDescription in jetson launch file to avoid params_file cross talk

--- bringup/jetson/roland.launch.py
+++ bringup/jetson/roland.launch.py
@@ -9,22 +9,32 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    gimbal_pkg_dir = get_package_share_directory('gimbal_control')
-    base_pkg_dir = get_package_share_directory('base_control')
-
-    gimbal_launch = os.path.join(gimbal_pkg_dir, 'launch', 'gimbal_controller.launch.py')
-    base_launch = os.path.join(base_pkg_dir, 'launch', 'base_controller.launch.py')
-
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gimbal_launch),
+        GroupAction(
+            actions=[
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('gimbal_control'),
+                            'launch', 'gimbal_controller.launch.py')),
+                ),
+            ],
+            forwarding=False
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(base_launch),
+        GroupAction(
+            actions=[
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(
+                            get_package_share_directory('base_control'),
+                            'launch', 'base_controller.launch.py')),
+                ),
+            ],
+            forwarding=False
         ),
     ])
